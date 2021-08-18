cd2b71793515a40003d2adcfd4cb77aab5d6b461
basselalsayed <alsayed.bassel@gmail.com>
2021-08-18 07:46:49 +0100

    utils dir and Error class

---
+++ src/utils/Error.js
@@ -0,0 +1,45 @@
+class GeneralError extends Error {
+  constructor(message) {
+    super();
+    this.message = message;
+  }
+
+  getCode() {
+    if (this instanceof BadRequest) {
+      return 400;
+    }
+    if (this instanceof Forbidden) {
+      return 403;
+    }
+    if (this instanceof Conflict) {
+      return 409;
+    }
+    if (this instanceof NotFound) {
+      return 404;
+    }
+    if (this instanceof NotUpdated) {
+      return 404;
+    }
+    if (this instanceof Unauthorized) {
+      return 401;
+    }
+    return 500;
+  }
+}
+
+class BadRequest extends GeneralError {}
+class Conflict extends GeneralError {}
+class Forbidden extends GeneralError {}
+class NotFound extends GeneralError {}
+class NotUpdated extends GeneralError {}
+class Unauthorized extends GeneralError {}
+
+export {
+  GeneralError,
+  Conflict,
+  Forbidden,
+  BadRequest,
+  NotFound,
+  NotUpdated,
+  Unauthorized,
+};
