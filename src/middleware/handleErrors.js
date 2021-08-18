1a182d84c6e40f2bed2f384324a400a489b9057c
basselalsayed <alsayed.bassel@gmail.com>
2021-08-18 02:22:19 +0100

    middleware directory and handleErrors middleware

---
+++ src/middleware/handleErrors.js
@@ -0,0 +1,14 @@
+const handleErrors = (error, { method, params, url }, res, next) => {
+  console.log(red(new Date(Date.now())));
+  console.log(red('[Error]:'), yellow(error));
+  console.log(red('[Request URL]:'), yellow(url));
+  console.log(red('[Request Method]:'), yellow(method));
+  console.log(red('[Request Params]:'), yellow(params));
+  console.log(yellow(error.stack));
+
+  if (error instanceof GeneralError) return send(error.getCode(), res, error);
+
+  return send(500, res, error);
+};
+
+export default handleErrors;
