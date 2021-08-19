c510d5107848c1a2cbaefeb83e910b67f20399a7
basselalsayed <alsayed.bassel@gmail.com>
2021-08-19 17:53:13 +0100

    use send helper in digitsController

--- src/controllers/digits.js
+++ src/controllers/digits.js
@@ -1,8 +1,8 @@
-import { getPossibleInputs } from '../helpers';
+import { getPossibleInputs, send } from '../helpers';
 
 const digitsController = async ({ body: { digits } }, res, next) => {
   try {
-    res.status(200).send(getPossibleInputs(digits));
+    send(200, res, getPossibleInputs(digits));
     next();
   } catch (error) {
     next(error);
